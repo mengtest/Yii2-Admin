dfefc16b5aea254b0ca69acee3441d55d6316158
wyx <1822581649@qq.com>
2019-03-10 22:25:23 +0800

    init

---
+++ common/config/main-local.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2-admin',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'tablePrefix' => 'yii2_',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.163.com',
+                'username' => '17725027209@163.com',
+                'password' => '987654321qqq',
+                'port' => '465',
+                'encryption' => 'ssl',
+            ],
+            'messageConfig' => [
+                'charset' => 'UTF-8',
+                'from' => ['17725027209@163.com' => 'admin']
+            ],
+        ],
+    ],
+];
